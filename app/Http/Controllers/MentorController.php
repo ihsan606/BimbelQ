e7916268b23ad0e5c3bf383e9bdb9ea25777d74b
fawwaz <20523125@students.uii.ac.id>
2023-01-26 18:58:42 +0700

    add app

---
+++ app/Http/Controllers/MentorController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tentor;
+use Illuminate\Http\Request;
+
+class MapelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tentor = Tentor::when(request()->q, function($tentor) {
+            $tentor = $tentor->where('mapels_name', 'like', '%'. request()->q . '%');
+        })->latest('id')->get();
+
+        return inertia('Tentors/Index', [
+            'tentors' => $tentor
+        ]);
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'tentor_email'      => 'required|email|unique:tentos',
+            'tentor_password'   => 'required|confirmed',
+            'tentor_nama'   => 'required',
+        ]);
+
+        $class = Mapel::create([
+            'tentor_email'      => $request->tentor_email,
+            'tentor_password'   => bcrypt($request->tentor_password),
+            'tentor_nama'   => $request->tentor_nama,
+        ]);
+
+        return $class;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $tentor = Tentor::findOrFail($id);
+
+        return $mapels;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'tentor_email'      => 'required|email|unique:tentos',
+            'tentor_password'   => 'required|confirmed',
+            'tentor_nama'   => 'required',,
+        ]);
+
+        $tentor = Mapel::findOrFail($id);
+
+
+
+
+
+        //update tentor
+        $tentor->update([
+            'tentor_email'      => $request->tentor_email,
+            'tentor_password'   => bcrypt($request->tentor_password),
+            'tentor_nama'   => $request->tentor_nama,
+        ]);
+
+        return $tentor;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $tentor = Mentor::findOrFail($id);
+
+        $tentor->delete();
+
+        if($tentor){
+            return "sukses dihapus";
+        }
+    }
+}
---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers\Apps;
+
+use Inertia\Inertia;
+use App\Models\Siswa;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //get siswas
+        $siswas = Siswa::when(request()->q, function($siswas) {
+            $siswas = $siswas->where('name', 'like', '%'. request()->q . '%');
+        })->latest()->paginate(5);
+
+        //return inertia
+        return inertia('Siswas/Index', [
+            'siswas' =>$siswas
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Apps/Siswas/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        /**
+         * validate
+         */
+        $this->validate($request, [
+            'siswa_email'      => 'required|email|unique:siswas',
+            'siswa_password'   => 'required|confirmed',
+            'siswa_nama'   => 'required',
+        ]);
+
+        //create siswa
+        Siswa::create([
+            'siswa_email'      => $request->siswa_email,
+            'siswa_password'   => bcrypt($request->siswa_password),
+            'siswa_nama'   => $request->siswa_nama,
+        ]);
+
+        //redirect
+        return redirect()->route('apps.siswas.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $siswas = Siswa::findOrFail($id);
+
+        return $siswas;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        /**
+         * validate
+         */
+        $this->validate($request, [
+            'siswa_email'      => 'required|email|unique:siswas',
+            'siswa_password'   => 'required|confirmed',
+            'siswa_nama'   => 'required',
+        ]);
+
+        $siswas = Siswa::findOrFail($id);
+
+        //update siswas
+        $siswas->update([
+            'siswa_email'      => $request->siswa_email,
+            'siswa_password'   => bcrypt($request->siswa_password),
+            'siswa_nama'   => $request->siswa_nama,
+        ]);
+
+        //redirect
+        return $siswas;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $siswas = Siswa::findOrFail($id);
+
+        $siswas->delete();
+
+        if($siswas){
+            return "sukses dihapus";
+        }
+    }
+}
