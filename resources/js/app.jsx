39233ed01b52dee69e65a857c60dde5066305f64
Yustina Yasin <yustinayasin@gmail.com>
2023-01-26 09:47:02 +0700

    tambah config postcss

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { createInertiaApp } from '@inertiajs/inertia-react';
 import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import '../css/app.css'
 
 createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
