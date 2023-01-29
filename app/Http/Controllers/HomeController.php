f9507865f25e124b203640540637a60d358c391e
Yustina Yasin <yustinayasin@gmail.com>
2023-01-30 01:22:59 +0700

    configurasi home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Siswa;
+use App\Models\Tentor;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     function index(){
-        return inertia('Home/Index');
+        $siswas = Siswa::when(request()->q, function($siswas) {
+            $siswas = $siswas->where('siswa_name', 'like', '%'. request()->q . '%');
+        })->latest('id')->get();
+
+        $tentors = Tentor::when(request()->q, function($tentors) {
+            $tentors = $tentors->where('tentors_name', 'like', '%'. request()->q . '%');
+        })->latest('id')->get();
+ 
+         return inertia('Home/Index', [
+             'siswas' => $siswas,
+             'tentors' => $tentors
+         ]);
     }
 }
