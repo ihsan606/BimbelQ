f246a34166aedfed8151292940601c1454b2d7ec
Muhammad Hafidzul Fikri <mhfikri2000@gmail.com>
2023-01-30 00:22:48 +0800

    add tagihan

--- resources/js/Layouts/SidebarNew.jsx
+++ resources/js/Layouts/SidebarNew.jsx
@@ -590,6 +590,66 @@ const SidebarNew = ({ children }) => {
                                     <span className="text-sm ml-2">Jadwal Bimbingan</span>
                                 </Link>
                             </li>
+                            <li
+                                className={
+                                    currentPath === "tagihan"
+                                        ? "flex w-full justify-between text-gray-300 cursor-pointer items-center mb-2 bg-gray-500 rounded-lg px-2 py-2"
+                                        : "flex w-full justify-between text-gray-300 cursor-pointer items-center mb-2 px-2 py-2 hover:bg-gray-500 hover:rounded-lg"
+                                }
+                            >
+                                <Link
+                                    href="/tagihan"
+                                    className="flex items-center focus:outline-none hover:text-white w-full focus:ring-2 focus:ring-white"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="icon icon-tabler icon-tabler-grid"
+                                        width="18"
+                                        height="18"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth="1.5"
+                                        stroke="currentColor"
+                                        fill="none"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                    >
+                                        <path
+                                            stroke="none"
+                                            d="M0 0h24v24H0z"
+                                        ></path>
+                                        <rect
+                                            x="4"
+                                            y="4"
+                                            width="6"
+                                            height="6"
+                                            rx="1"
+                                        ></rect>
+                                        <rect
+                                            x="14"
+                                            y="4"
+                                            width="6"
+                                            height="6"
+                                            rx="1"
+                                        ></rect>
+                                        <rect
+                                            x="4"
+                                            y="14"
+                                            width="6"
+                                            height="6"
+                                            rx="1"
+                                        ></rect>
+                                        <rect
+                                            x="14"
+                                            y="14"
+                                            width="6"
+                                            height="6"
+                                            rx="1"
+                                        ></rect>
+                                    </svg>
+                                    <span className="text-sm ml-2">Tagihan Siswa</span>
+                                </Link>
+                            </li>
+
 
                             <button
                                 onClick={logoutHandler}
